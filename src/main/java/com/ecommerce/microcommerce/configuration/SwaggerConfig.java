acb3a04a79b2741d8cc9151e66b2b8f3739427b3
Sanaa Boualila <boualilasanaa@gmail.com>
2023-12-19 05:43:46 -0800

    microservice de gestion des produits

---
+++ src/main/java/com/ecommerce/microcommerce/configuration/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.ecommerce.microcommerce.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.ecommerce.microcommerce.web"))
+                //Nous utilisons la méthode basePackage du prédicat RequestHandlerSelectors afin
+                //de demander à Swagger de ne rien documenter en dehors du package "web" qui contient notre code.
+                .paths(PathSelectors.regex("/Produits.*"))
+                //PathSelectors.regex("/Produits.*") permet de passer une expression régulière qui n'accepte
+                // que les URI commençant par /Produits.
+                .build();
+    }
+
+}
---
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -0,0 +1,78 @@
+package com.ecommerce.microcommerce.web.controller;
+
+import com.ecommerce.microcommerce.dao.ProductDao;
+import com.ecommerce.microcommerce.model.Product;
+import com.ecommerce.microcommerce.web.exception.ProduitIntrouvableException;
+import javax.validation.Valid;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+
+import java.net.URI;
+import java.util.List;
+import java.util.Objects;
+
+@RestController
+public class ProductController {
+
+    private final ProductDao productDao;
+
+    public ProductController(ProductDao prodDao){
+        this.productDao = prodDao;
+    }
+
+    @GetMapping("/Produits")
+    public List<Product> listeProduits() {
+        return productDao.findAll();
+    }
+
+    @ApiOperation(value = "Récupère un produit grâce à son ID à condition que celui-ci soit en stock!")
+    @GetMapping(value = "/Produits/{id}")
+    public Product afficherUnProduit(@PathVariable int id) {
+        Product produit = productDao.findById(id);
+        if(produit==null) throw new ProduitIntrouvableException("Le produit avec l'id " + id + " est INTROUVABLE. Écran Bleu si je pouvais.");
+        return produit;
+    }
+
+    @GetMapping(value = "/test/Produits/{prixLimit}")
+    public List<Product> testRequete(@PathVariable int prixLimit){
+        return productDao.findByPrixGreaterThan(prixLimit);
+    }
+
+
+    @PostMapping(value = "/Produits")
+    public ResponseEntity<Product> ajouterProduit(@Valid @RequestBody Product product) {
+        Product productAdded = productDao.save(product);
+        if (Objects.isNull(productAdded)) {
+            return ResponseEntity.noContent().build();
+        }
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(productAdded.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+    @PutMapping(value="/Produits")
+    public Product updateProduit(@RequestBody Product product){
+        return productDao.save(product);
+    }
+
+
+    @DeleteMapping(value = "/Produits/{id}")
+    public void deleteProduit(@PathVariable int id) {
+        productDao.deleteById(id);
+    }
+
+
+
+
+
+
+
+}
